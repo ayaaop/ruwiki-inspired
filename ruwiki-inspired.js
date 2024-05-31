9d4a6639818a50c2d6f5c3c7f69caf8ac15c7e07
ayato <58212796+ayaaop@users.noreply.github.com>
2024-05-31 21:02:27 +0000

    enhance
    ### Changes Made:
    - Updated the version to 0.9.
    - Added functionality to initialize collapsible elements inside portable infoboxes when moved to the right rail.
    - Implemented wrapping of overflowing elements for better presentation (sometimes Fandom doesn't do that).
    - Introduced a dummy element to detect when the Table of Contents (ToC) is at the top.
    - Utilised IntersectionObserver to monitor the sentinel element for ToC scrolling behaviour.
    - Wrapped hidden categories in a container and included a button to toggle their visibility.
    - Added NoGlobalNav import

--- ruwiki-inspired.js
+++ ruwiki-inspired.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Fandom Article Formatter
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.3
 // @description  Format Fandom articles and check the ToC checkbox
 // @author       copilot and gpt4o
 // @match        *://*.fandom.com/*
 // @require      https://code.jquery.com/jquery-3.6.1.min.js
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function() {
@@ -14,19 +14,24 @@
 
     $(document).ready(function() {
         var $body = $('body');
-        var $mwOutput = $('.mw-body-content > .mw-parser-output');
+        var $mwBodyContent = $('.mw-body-content');
+        var $mwParserOutput = $mwBodyContent.children('.mw-parser-output');
         var $pageHeader = $('.page-header').detach();
-        var $toc = $mwOutput.find('.toc').find('input.toctogglecheckbox').prop('checked', true).end();
-        var $infobox = $mwOutput.find('.portable-infobox');
+        var $toc = $mwParserOutput.find('.toc').find('input.toctogglecheckbox').prop('checked', true).end();
+        var $infobox = $mwParserOutput.find('.portable-infobox');
         var $tocPrev = $toc.prev();
         var $infoboxPrev = $infobox.prev();
         var $section;
 
+        if (!$mwParserOutput.length) {
+            $mwParserOutput = $('<div class="mw-parser-output"></div>').appendTo($mwBodyContent);
+            $mwBodyContent.contents().appendTo($mwParserOutput);
+        }
         if ($body.hasClass('mainpage')) {
-            $mwOutput.wrapInner('<section class="preamble-section"></section>').find('.preamble-section').prepend($pageHeader);
+            $mwParserOutput.wrapInner('<section class="article-section"></section>').find('.article-section').prepend($pageHeader);
         } else {
-            $section = $('<section class="preamble-section"></section>').prepend($pageHeader).prependTo($mwOutput);
-            $mwOutput.contents().each(function() {
+            $section = $('<section class="preamble-section"></section>').prepend($pageHeader).prependTo($mwParserOutput);
+            $mwParserOutput.contents().each(function() {
                 var $this = $(this);
                 if (this.nodeType === 3 && $.trim(this.nodeValue)) {
                     $section.append(this);
@@ -37,10 +42,6 @@
                 }
             });
         }
-
-        $(window).scroll(function() {
-            $toc.toggleClass('scrolled', $(this).scrollTop() > 0);
-        });
 
         function updateLayout() {
             var $rightRail = $('.page__right-rail');
@@ -54,11 +55,102 @@
                 $infobox.detach().addClass('rail-module').prependTo($rightRailWrapper);
                 $body.addClass('right-rail-visible');
             }
+
+            initializeCollapsibleElements();
+            wrapOverflowingElements();
+
+        }
+
+        function initializeCollapsibleElements() {
+            $('.mw-collapsible').each(function() {
+                var $this = $(this);
+
+                if (!$this.hasClass('mw-made-collapsible')) {
+                    var expandText = $this.data('expandtext') || 'Expand';
+                    var collapseText = $this.data('collapsetext') || 'Collapse';
+
+                    // Create the toggle if it doesn't exist
+                    if (!$this.find('.mw-collapsible-toggle').length) {
+                        var $toggle = $('<span class="mw-collapsible-toggle mw-collapsible-toggle-default mw-collapsible-toggle-collapsed" role="button" tabindex="0" aria-expanded="false"><a class="mw-collapsible-text">' + expandText + '</a></span>');
+                        $toggle.on('click', function() {
+                            var $content = $this.find('.mw-collapsible-content');
+                            if ($this.hasClass('mw-collapsed')) {
+                                $this.removeClass('mw-collapsed').addClass('mw-expanded');
+                                $content.slideDown();
+                                $toggle.attr('aria-expanded', 'true').removeClass('mw-collapsible-toggle-collapsed').addClass('mw-collapsible-toggle-expanded').find('.mw-collapsible-text').text(collapseText);
+                            } else {
+                                $this.removeClass('mw-expanded').addClass('mw-collapsed');
+                                $content.slideUp();
+                                $toggle.attr('aria-expanded', 'false').removeClass('mw-collapsible-toggle-expanded').addClass('mw-collapsible-toggle-collapsed').find('.mw-collapsible-text').text(expandText);
+                            }
+                        });
+                        $this.prepend($toggle);
+                    }
+
+                    // Wrap the content if it doesn't exist
+                    if (!$this.find('.mw-collapsible-content').length) {
+                        var $content = $this.children().not('.mw-collapsible-toggle').wrapAll('<div class="mw-collapsible-content" style="display: none;"></div>').parent();
+                    }
+
+                    $this.addClass('mw-made-collapsible');
+                }
+            });
+        }
+        function wrapOverflowingElements() {
+            $mwParserOutput.children().each(function() {
+                var $this = $(this);
+                if ($this.width() > $mwParserOutput.width()) {
+                    $this.wrap('<div class="scrollable"></div>');
+                }
+            });
         }
 
         updateLayout();
         $('.right-rail-toggle').click(function() {
             setTimeout(updateLayout, 0);
         });
+
+        // Create a dummy element to detect when .toc is at the top
+        var $tocSentinel = $('<div></div>').insertBefore($toc);
+
+        // Create IntersectionObserver to observe the sentinel element
+        var observer = new IntersectionObserver(function(entries) {
+            entries.forEach(function(entry) {
+                if (entry.intersectionRatio === 0) {
+                    $toc.addClass('scrolled');
+                } else {
+                    $toc.removeClass('scrolled');
+                }
+            });
+        }, { threshold: [0, 1] });
+
+        // Observe the sentinel element
+        observer.observe($tocSentinel[0]);
+
+        // Wrap the hidden categories and initially hide them
+        var $hiddenCategoriesLabel = $('.page-footer__categories .hidden-categories-label');
+        var $hiddenCategoriesList = $hiddenCategoriesLabel.next('ul.categories');
+        var $hiddenCategoriesContainer = $('<div class="hidden-categories-container" style="display: none;"></div>');
+        if ($hiddenCategoriesLabel.length && $hiddenCategoriesList.length) {
+            $hiddenCategoriesLabel.add($hiddenCategoriesList).wrapAll($hiddenCategoriesContainer);
+
+            // Add a button to toggle hidden categories visibility
+            var $toggleButton = $('<button class="toggle-hidden-categories">Show Hidden Categories</button>');
+            $toggleButton.insertAfter('.page-footer__categories .special-categories-label');
+
+            $toggleButton.click(function() {
+                var $hiddenContainer = $('.hidden-categories-container');
+                if ($hiddenContainer.is(':visible')) {
+                    $hiddenContainer.hide();
+                    $(this).text('Show Hidden Categories');
+                } else {
+                    $hiddenContainer.show();
+                    $(this).text('Hide Hidden Categories');
+                }
+            });
+        }
+
+        mw.loader.load('https://dev.fandom.com/load.php?mode=articles&articles=MediaWiki:NoGlobalNav.js');
+        mw.loader.load('https://dev.fandom.com/load.php?mode=articles&articles=MediaWiki:NoGlobalNav.css&only=styles', "text/css");
     });
 })();
